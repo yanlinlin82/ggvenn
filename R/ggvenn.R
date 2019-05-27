8e6c83bf93eee5fa0122bef9a56a91d6c59f23ce
Linlin Yan <yanlinlin82@gmail.com>
2019-05-27 21:16:51 +0800

    Correct help document

--- R/ggvenn.R
+++ R/ggvenn.R
@@ -10,15 +10,15 @@
 #'           `List 3` = c(1, 2, 8, 9),
 #'           `List 4` = c(6, 7, 10, 12))
 #' ggvenn(a, c("List 1", "List 2"))
-#' ggvenn(a, c("List 1", "List 2", "List 3")))
+#' ggvenn(a, c("List 1", "List 2", "List 3"))
 #' ggvenn(a)
 #'
-#' d <- data.frame(value = c(1, 2, 3, 5, 6, 7, 8, 9, 10, 12, 13),
-#'                 `List 1` = c(T, F, T, T, F, T, F, T, F,  F,  F),
-#'                 `List 2` = c(T, F, F, T, F, F, F, T, F,  F,  T),
-#'                 `List 3` = c(T, T, F, F, F, F, T, T, F,  F,  F),
-#'                 `List 4` = c(F, F, F, F, T, T, F, F, T,  T,  F))
-#' ggvenn(d, c("List 1"))
+#' d <- tibble(value    = c(1, 2, 3, 5, 6, 7, 8, 9, 10, 12, 13),
+#'             `List 1` = c(T, F, T, T, F, T, F, T, F,  F,  F),
+#'             `List 2` = c(T, F, F, T, F, F, F, T, F,  F,  T),
+#'             `List 3` = c(T, T, F, F, F, F, T, T, F,  F,  F),
+#'             `List 4` = c(F, F, F, F, T, T, F, F, T,  T,  F))
+#' ggvenn(d, c("List 1", "List 2"))
 #' ggvenn(d, c("List 1", "List 2", "List 3"))
 #' ggvenn(d)
 ggvenn <- function(data, columns = NULL) {
