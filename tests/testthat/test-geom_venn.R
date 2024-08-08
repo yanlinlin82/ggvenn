9e6414bc301dbd8682ef4e3f34a92921c57299fa
Daniel E. de la Rosa <drdsdaniel@gmail.com>
2024-08-08 00:06:47 -0400

    test: add some tests before refactoring the code
    I have added all the examples from the description and some additional ones to test the code.

---
+++ tests/testthat/test-geom_venn.R
@@ -0,0 +1,29 @@
+test_that("geom_venn", {
+  
+  d <- tibble(value   = c(1,     2,     3,     5,     6,     7,     8,     9),
+              `Set 1` = c(TRUE,  FALSE, TRUE,  TRUE,  FALSE, TRUE,  FALSE, TRUE),
+              `Set 2` = c(TRUE,  FALSE, FALSE, TRUE,  FALSE, FALSE, FALSE, TRUE),
+              `Set 3` = c(TRUE,  TRUE,  FALSE, FALSE, FALSE, FALSE, TRUE,  TRUE),
+              `Set 4` = c(FALSE, FALSE, FALSE, FALSE, TRUE,  TRUE,  FALSE, FALSE))
+  
+  # draw two-set venn (use A, B in aes)
+  expect_snapshot(
+  ggplot_build(
+    ggplot(d, aes(A = `Set 1`, B = `Set 2`)) +
+    geom_venn() + theme_void() + coord_fixed())$data
+  )
+  
+  # draw three-set venn (use A, B, C in aes)
+  expect_snapshot(
+  ggplot_build(
+  ggplot(d, aes(A = `Set 1`, B = `Set 2`, C = `Set 3`)) +
+    geom_venn() + theme_void() + coord_fixed())$data
+  )
+  
+  # draw four-set venn (use A, B, C, D in aes)
+  expect_snapshot(
+  ggplot_build(
+  ggplot(d, aes(A = `Set 1`, B = `Set 2`, C = `Set 3`, D = `Set 4`)) +
+    geom_venn() + theme_void() + coord_fixed())$data
+  )
+})
---
+++ tests/testthat/test-ggvenn.R
@@ -0,0 +1,35 @@
+test_that("ggvenn + list", {
+  #library(ggvenn)
+  
+  a <- list(`Set 1` = c(1, 3, 5, 7, 9),
+            `Set 2` = c(1, 5, 9, 13),
+            `Set 3` = c(1, 2, 8, 9),
+            `Set 4` = c(6, 7, 10, 12))
+  
+  expect_snapshot(
+    ggplot_build(ggvenn(a, c("Set 1", "Set 2")))$data            # draw two-set venn
+  )
+  
+ expect_snapshot(
+    ggplot_build(ggvenn(a, c("Set 1", "Set 2", "Set 3")))$data   # draw three-set venn
+ )
+ 
+ expect_snapshot(
+ ggplot_build(ggvenn(a))$data   # without set names, the first 4 elements in list will be chose to draw four-set venn
+ )
+})
+
+
+
+test_that("ggvenn + data.frame", {
+  
+  d <- dplyr::tibble(value   = c(1,     2,     3,     5,     6,     7,     8,     9),
+              `Set 1` = c(TRUE,  FALSE, TRUE,  TRUE,  FALSE, TRUE,  FALSE, TRUE),
+              `Set 2` = c(TRUE,  FALSE, FALSE, TRUE,  FALSE, FALSE, FALSE, TRUE),
+              `Set 3` = c(TRUE,  TRUE,  FALSE, FALSE, FALSE, FALSE, TRUE,  TRUE),
+              `Set 4` = c(FALSE, FALSE, FALSE, FALSE, TRUE,  TRUE,  FALSE, FALSE))
+  
+  expect_snapshot(ggvenn(d, c("Set 1", "Set 2"))$data)           # draw two-set venn
+  expect_snapshot(ggvenn(d, c("Set 1", "Set 2", "Set 3"))$data)  # draw three-set venn
+  expect_snapshot(ggvenn(d)$data)   # without set names, the first 4 logical column in data.frame will be chose to draw four-set venn
+})
