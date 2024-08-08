231f99cae013e18b467139740ae81ccf11d7163e
Daniel E. de la Rosa <drdsdaniel@gmail.com>
2024-08-08 01:22:11 -0400

    test: Update snapshot tests to include deprecation warning
    This commit updates the snapshot tests for the `ggvenn` package to include the deprecation warning for the `show_percentage` argument.
    
    The `show_percentage` argument has been deprecated in favor of the more versatile `show_stats` argument. This update ensures that the snapshot tests accurately reflect the current behavior of the package, including the warning message that is displayed when using the deprecated argument.
    
    The updated snapshots now include the following warning:
    
    ```
    Warning <lifecycle_warning_deprecated>
      The `show_percentage` argument of `ggvenn()` is deprecated as of ggvenn 0.1.11.
      i Please use the `show_stats` argument instead.
    ```

---
+++ tests/testthat/test-stats.R
@@ -0,0 +1,17 @@
+test_that("stats", {
+  a <- list(
+    `Set 1` = c(1, 3, 5, 7, 9),
+    `Set 2` = c(1, 5, 9, 13),
+    `Set 3` = c(1, 2, 8, 9),
+    `Set 4` = c(6, 7, 10, 12)
+  )
+  
+  expect_snapshot(ggplot_build(ggvenn(a))$data)
+  expect_snapshot(ggplot_build(ggvenn(a, show_percentage = FALSE))$data)
+  expect_snapshot(ggplot_build(ggvenn(a, show_percentage = TRUE))$data)
+  expect_snapshot(ggplot_build(ggvenn(a, show_percentage = TRUE, show_stats = 'p'))$data)
+  expect_snapshot(ggplot_build(ggvenn(a, show_percentage = TRUE, show_stats = 'p'))$data)
+  expect_snapshot(ggplot_build(ggvenn(a, show_stats = 'c'))$data)
+  expect_snapshot(ggplot_build(ggvenn(a, show_stats = 'p'))$data)
+  expect_snapshot(ggplot_build(ggvenn(a, show_stats = 'cp'))$data)
+})
